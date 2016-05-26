f86b396553de5e0dac684c6c2ff5fb13dfba7298
Brian Mancini <bmancini@gmail.com>
2016-05-26 13:13:24 -0400

    Added tests for toJSON of objects with buffers

---
+++ buffer-to-object-01.js
@@ -0,0 +1,33 @@
+
+const fs = require('fs')
+const path = 'fixtures/test.jpg';
+
+setInterval(run, 500);
+
+function run() {
+  readFileAsync(path)
+  .then(file => createObjectBuffer(path, file))
+  .then(buffer => {
+    let result = JSON.parse(buffer.toString());
+    result.buffer = new Buffer(result.buffer);
+    console.log(process.memoryUsage());
+
+    if(global.gc)
+      global.gc();
+  });
+}
+
+function readFileAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, buffer) => {
+      if(err) reject(err);
+      else    resolve(buffer);
+    });
+  });
+}
+
+function createObjectBuffer(path, buffer) {
+  let json   = JSON.stringify({ path, buffer });
+  let result = new Buffer(json);
+  return result;
+}
