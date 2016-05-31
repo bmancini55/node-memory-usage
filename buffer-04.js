895efaa61afa3d1ac9cf8050be56fe24799ced45
Brian Mancini <bmancini@gmail.com>
2016-05-30 21:59:21 -0400

    Fixing buffer test 04

--- buffer-04.js
+++ buffer-04.js
@@ -16,14 +16,17 @@ function run() {
 function fetchFile(url, cb) {
   let buffer;
   let offset = 0;
+  let byte;
   request
     .get(url)
     .on('response', (r) => {
       buffer = new Buffer(parseInt(r.headers['content-length']));
     })
     .on('data',  d => {
-      buffer.writeInt8(d, offset);
-      offset += d.length;
+      for(byte of d) {
+        buffer.writeUInt8(byte, offset);
+        offset += 1;
+      }
     })
     .on('error', e => cb(e))
     .on('end', () => cb(null, buffer));
