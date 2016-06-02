33da743b153455f7bce91025deb117a142f68545
Brian Mancini <bmancini@gmail.com>
2016-06-02 14:50:48 -0400

    Added buffer.allocUnsafe test

---
+++ buffer-06.js
@@ -0,0 +1,31 @@
+/**
+ * Sized buffer, with offset insert
+ */
+
+let fs = require('fs');
+
+setInterval(run, 1000);
+
+function run() {
+  fetchFile((err, buffer) => {
+    console.log(err || process.memoryUsage());
+  });
+}
+
+function fetchFile(cb) {
+  let buffer;
+  let offset = 0;
+  let { size } = fs.lstatSync('fixtures/test.jpg')
+
+  require('fs')
+    .createReadStream('fixtures/test.jpg')
+    .on('open', () => buffer = Buffer.allocUnsafe(size))
+    .on('data',  d => {
+      for(let i = 0; i < d.length; i += 1) {
+        buffer.writeUInt8(d[i], offset);
+        offset += 1;
+      }
+    })
+    .on('error', e => cb(e))
+    .on('end', () => cb(null, buffer));
+}
